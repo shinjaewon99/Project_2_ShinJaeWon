832a08d8aacdccddc30ad439e62b876b7508ed4e
shinjaewon99 <tlswodnjs99@naver.com>
2023-08-03 22:22:22 +0900

    feat : token 응답 DTO 클래스 추가 #8

---
+++ src/main/java/com/likelion/sns/auth/dto/response/AuthenticationResponse.java
@@ -0,0 +1,12 @@
+package com.likelion.sns.auth.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class AuthenticationResponse {
+    private String token;
+}
