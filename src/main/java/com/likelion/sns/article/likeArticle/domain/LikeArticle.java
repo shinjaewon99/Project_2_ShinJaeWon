12cf1ff2e361541eca99e511c3c4212dfe85725f
shinjaewon99 <tlswodnjs99@naver.com>
2023-08-04 15:32:48 +0900

    feat : LikeArticle 연관관계 매핑 엔티티 클래스 추가 #20

---
+++ src/main/java/com/likelion/sns/article/likeArticle/domain/LikeArticle.java
@@ -0,0 +1,28 @@
+package com.likelion.sns.article.likeArticle.domain;
+
+import com.likelion.sns.article.feed.domain.Feed;
+import com.likelion.sns.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "like_article")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LikeArticle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "like_article_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feed_id")
+    private Feed feedId;
+}
