ba67e285b98057713c548c15ff628aa00877b750
shinjaewon99 <tlswodnjs99@naver.com>
2023-08-05 23:17:25 +0900

    feat : 논리 삭제에 필요한 어노테이션 추가 #30

--- src/main/java/com/likelion/sns/article/feed/domain/Feed.java
+++ src/main/java/com/likelion/sns/article/feed/domain/Feed.java
@@ -1,5 +1,7 @@
 package com.likelion.sns.article.feed.domain;
 
+import com.likelion.sns.article.articleImage.domain.ArticleImage;
+import com.likelion.sns.article.feed.dto.request.CreateFeedRequest;
 import com.likelion.sns.article.likeArticle.domain.LikeArticle;
 import com.likelion.sns.global.entity.BaseEntity;
 import com.likelion.sns.user.domain.User;
@@ -7,8 +9,12 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.Where;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,8 +22,9 @@ import java.util.List;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE feeds SET deleted_at = CURRENT_TIMESTAMP where feed_id = ?") // delete 쿼리문을 작성
+@Where(clause = "deleted_at is null")
 public class Feed extends BaseEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "feed_id")
@@ -39,11 +46,32 @@ public class Feed extends BaseEntity {
     @Column(name = "deleted_at")
     private LocalDateTime deleteDate;
 
-    @OneToMany(mappedBy = "feedId")
+    @OneToMany(mappedBy = "imageFeedId")
+    private List<ArticleImage> articleImages = new ArrayList<>();
+
+    @OneToMany(mappedBy = "likeFeedId")
     private List<LikeArticle> feedIdLikeArticles = new ArrayList<>();
 
     public void setUser(User user) {
         this.userId = user;
-        user.addFeed(this);
+        user.getFeeds().add(this);
+    }
+
+    public void addArticleImage(ArticleImage articleImage) {
+        articleImages.add(articleImage);
+        articleImage.setFeed(this);
+    }
+
+    public void addLikeArticle(LikeArticle likeArticle) {
+        feedIdLikeArticles.add(likeArticle);
+        likeArticle.setFeed(this);
+    }
+
+    public static Feed dtoToEntity(CreateFeedRequest createRequest) {
+        Feed feed = new Feed();
+        feed.title = createRequest.getTitle();
+        feed.content = createRequest.getContent();
+
+        return feed;
     }
 }
