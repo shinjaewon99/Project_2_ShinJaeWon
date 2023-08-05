47a1752bc90e6889df1a97ddc9ae65f9d2cb142f
shinjaewon99 <tlswodnjs99@naver.com>
2023-08-06 00:33:44 +0900

    feat : 아이디 미입력 예외 클래스 추가 #35

---
+++ src/main/java/com/likelion/sns/user/exception/NotInputUserIdException.java
@@ -0,0 +1,7 @@
+package com.likelion.sns.user.exception;
+
+public class NotInputUserIdException extends RuntimeException {
+    public NotInputUserIdException(final String message) {
+        super(message);
+    }
+}
