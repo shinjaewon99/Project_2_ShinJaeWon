f671825ee3ae4a13863bf13e190452d584be5834
shinjaewon99 <tlswodnjs99@naver.com>
2023-08-06 15:06:08 +0900

    refactor : 이미지 등록 메소드 추가 #39

--- src/main/java/com/likelion/sns/user/controller/UserController.java
+++ src/main/java/com/likelion/sns/user/controller/UserController.java
@@ -4,46 +4,33 @@ import com.likelion.sns.auth.dto.response.AuthenticationResponse;
 import com.likelion.sns.user.dto.request.UserJoinRequest;
 import com.likelion.sns.user.dto.request.logIn.UserLoginRequest;
 import com.likelion.sns.user.dto.response.UserCommonResponse;
-import com.likelion.sns.user.dto.response.UserReadResponse;
 import com.likelion.sns.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
-@Slf4j
 public class UserController {
     private final UserService userService;
-    private final UserDetailsService userDetailsService;
 
     @PostMapping("/join")
-    public ResponseEntity<UserCommonResponse> join(@RequestBody final UserJoinRequest joinRequest) {
+    public ResponseEntity<UserCommonResponse> join(@RequestBody @Valid final UserJoinRequest joinRequest) {
         return ResponseEntity.ok(userService.userJoin(joinRequest));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<AuthenticationResponse> login(@Valid @RequestBody final UserLoginRequest loginRequest) {
+    public ResponseEntity<AuthenticationResponse> login(@RequestBody final UserLoginRequest loginRequest) {
         return ResponseEntity.ok(userService.userLogin(loginRequest));
     }
 
-    @PostMapping(value = "/postProfile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<UserCommonResponse> getUserDetails(@RequestParam("image") final MultipartFile image,
-                                                             final Authentication authentication) {
-        String userId = authentication.getName();
-
-        return ResponseEntity.ok(userService.postImg(image, userId));
-    }
-
-    @GetMapping("/myProfile/read")
-    public UserReadResponse myProfileRead(final Authentication authentication) {
-        return userService.readUserOne(authentication);
+    @PostMapping(value = "/{userId}/postProfile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<UserCommonResponse> getUserDetails(@PathVariable final Long userId,
+                                                             @RequestParam("image") final MultipartFile image) {
+        return ResponseEntity.ok(userService.postImg(userId, image));
     }
 }
