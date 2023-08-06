60fff97d437d1bd0f7c09e5e04660ad71d3105bd
shinjaewon99 <tlswodnjs99@naver.com>
2023-08-06 15:39:39 +0900

    remove : 비슷한 성질의 예외 처리 클래스 삭제 #43

--- src/main/java/com/likelion/sns/user/exception/NotInputPasswordException.java
+++
@@ -1,7 +0,0 @@
-package com.likelion.sns.user.exception;
-
-public class NotInputPasswordException extends RuntimeException {
-    public NotInputPasswordException(final String message) {
-        super(message);
-    }
-}
--- src/main/java/com/likelion/sns/user/exception/NotInputUserIdException.java
+++
@@ -1,7 +0,0 @@
-package com.likelion.sns.user.exception;
-
-public class NotInputUserIdException extends RuntimeException {
-    public NotInputUserIdException(final String message) {
-        super(message);
-    }
-}
