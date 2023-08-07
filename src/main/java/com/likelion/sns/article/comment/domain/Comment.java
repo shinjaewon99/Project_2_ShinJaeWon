828a0f2f7a94539dc618d9f8d448f706ad9d722e
shinjaewon99 <tlswodnjs99@naver.com>
2023-08-07 19:01:39 +0900

    refactor : Comment 엔티티 클래스, 댓글 생성자, 연관관계 메소드 추가 #56

--- src/main/java/com/likelion/sns/article/comment/domain/Comment.java
+++ src/main/java/com/likelion/sns/article/comment/domain/Comment.java
@@ -1,17 +1,22 @@
 package com.likelion.sns.article.comment.domain;
 
 import com.likelion.sns.article.feed.domain.Feed;
+import com.likelion.sns.global.entity.BaseEntity;
 import com.likelion.sns.user.domain.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+
+import java.time.LocalDateTime;
 
 @Table(name = "comments")
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Comment {
+@SQLDelete(sql = "UPDATE comments SET deleted_at = CURRENT_TIMESTAMP where feed_id = ?")
+public class Comment extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "comment_id")
@@ -23,7 +28,32 @@ public class Comment {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "feed_id")
-    private Feed feedId;
+    private Feed commentFeedId;
 
+    @Column(name = "deleted_at")
+    private LocalDateTime deleteDate;
+
+    @Column(name = "writer")
+    private String writer;
+    @Column(name = "content")
     private String content;
+
+    public Comment(final String writer, final String content) {
+        this.writer = writer;
+        this.content = content;
+    }
+
+    public void setUser(final User user) {
+        this.userId = user;
+        userId.getUserComments().add(this);
+    }
+
+    public void setFeed(final Feed feed) {
+        this.commentFeedId = feed;
+        commentFeedId.getComments().add(this);
+    }
+
+    public void modifyComment(final String content) {
+        this.content = content;
+    }
 }
