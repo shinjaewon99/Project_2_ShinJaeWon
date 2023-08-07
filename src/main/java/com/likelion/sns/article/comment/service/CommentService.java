41160e7879df17d42ad1daa13a6a1b8f1cd1b402
shinjaewon99 <tlswodnjs99@naver.com>
2023-08-07 12:56:24 +0900

    feat : 댓글 수정 기능 추가 #48

--- src/main/java/com/likelion/sns/article/comment/service/CommentService.java
+++ src/main/java/com/likelion/sns/article/comment/service/CommentService.java
@@ -2,6 +2,7 @@ package com.likelion.sns.article.comment.service;
 
 import com.likelion.sns.article.comment.domain.Comment;
 import com.likelion.sns.article.comment.domain.CommentRepository;
+import com.likelion.sns.article.comment.dto.request.CommentModifyRequest;
 import com.likelion.sns.article.comment.dto.request.CommentWriteRequest;
 import com.likelion.sns.article.comment.dto.response.CommentCommonResponse;
 import com.likelion.sns.article.feed.domain.Feed;
@@ -12,9 +13,11 @@ import com.likelion.sns.user.domain.User;
 import com.likelion.sns.user.domain.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class CommentService {
     private final UserRepository userRepository;
     private final FeedRepository feedRepository;
@@ -41,6 +44,26 @@ public class CommentService {
         return commentCommonResponse;
     }
 
+    // 댓글 수정 메소드
+    public CommentCommonResponse modifyComment(final Long userId, final Long feedId, final Long commentId,
+                                               final CommentModifyRequest modifyRequest) {
+        User findUser = validateExistUser(userId);
+        Feed findFeed = validateExistFeed(feedId);
+        Comment findComment = validateExistComment(commentId);
+        validateContent(modifyRequest.getContent());
+
+        findComment.setUser(findUser); // 연관관계 메소드 호출
+        findComment.setFeed(findFeed); // 연관관계 메소드 호출
+
+        findComment.modifyComment(modifyRequest.getContent());
+
+        CommentCommonResponse commentCommonResponse = new CommentCommonResponse();
+        commentCommonResponse.setMessage("댓글의 내용이 수정 되었습니다.");
+
+        return commentCommonResponse;
+    }
+
+
     private User validateExistUser(final Long userId) {
         return userRepository.findById(userId).orElseThrow(() -> new NotExistUserException("등록된 회원이 없습니다."));
     }
@@ -49,14 +72,18 @@ public class CommentService {
         return feedRepository.findById(feedId).orElseThrow(() -> new NotExistUserException("등록된 피드가 없습니다."));
     }
 
+    private Comment validateExistComment(final Long commentId) {
+        return commentRepository.findById(commentId).orElseThrow(() -> new NotExistUserException("등록된 피드가 없습니다."));
+    }
+
     private void validateWriter(String writer) {
-        if(writer == null){
+        if (writer == null) {
             throw new NotInputException("작성자를 입력해주세요.");
         }
     }
 
     private void validateContent(String content) {
-        if(content == null){
+        if (content == null) {
             throw new NotInputException("댓글 내용을 입력해주세요.");
         }
     }
