2c65d33c06b05780aca97f958fa674a67adfe276
shinjaewon99 <tlswodnjs99@naver.com>
2023-08-07 18:58:40 +0900

    feat : LikeArticle 레포지토리 추가 #53

---
+++ src/main/java/com/likelion/sns/article/likeArticle/domain/LikeArticleRepository.java
@@ -0,0 +1,6 @@
+package com.likelion.sns.article.likeArticle.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeArticleRepository extends JpaRepository<LikeArticle, Long> {
+}
