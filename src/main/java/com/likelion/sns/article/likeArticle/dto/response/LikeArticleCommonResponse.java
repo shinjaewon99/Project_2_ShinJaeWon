5c15099f66a95fa878d36c8858516cabe792ae66
shinjaewon99 <tlswodnjs99@naver.com>
2023-08-07 18:56:26 +0900

    feat : LikeArticle 공통 응답 DTO #50

---
+++ src/main/java/com/likelion/sns/article/likeArticle/dto/response/LikeArticleCommonResponse.java
@@ -0,0 +1,10 @@
+package com.likelion.sns.article.likeArticle.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LikeArticleCommonResponse {
+    private String message;
+}
