58bda60f890f13c38dc4673798c43085acd1da7f
shinjaewon99 <tlswodnjs99@naver.com>
2023-08-07 19:00:04 +0900

    refactor : LikeArticleController 좋아요 기능 추가 #55

---
+++ src/main/java/com/likelion/sns/article/likeArticle/controller/LikeArticleController.java
@@ -0,0 +1,24 @@
+package com.likelion.sns.article.likeArticle.controller;
+
+import com.likelion.sns.article.likeArticle.dto.response.LikeArticleCommonResponse;
+import com.likelion.sns.article.likeArticle.service.LikeArticleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/user/{userId}/feed/{feedId}/like")
+public class LikeArticleController {
+    private final LikeArticleService likeArticleService;
+
+    @PostMapping("/push")
+    public ResponseEntity<LikeArticleCommonResponse> likePush(@PathVariable final Long userId,
+                                                              @PathVariable final Long feedId) {
+
+        return ResponseEntity.ok().body(likeArticleService.likeFeed(userId, feedId));
+    }
+}
