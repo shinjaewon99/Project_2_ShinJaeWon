3388cc36e6a59b1f5c2cb85b2e03e3b918142883
shinjaewon99 <tlswodnjs99@naver.com>
2023-08-07 18:57:12 +0900

    feat : 나의 피드의 좋아요에 대한 예외 클래스 추가 #51

---
+++ src/main/java/com/likelion/sns/article/likeArticle/exception/NotMyFeedLikeException.java
@@ -0,0 +1,8 @@
+package com.likelion.sns.article.likeArticle.exception;
+
+public class NotMyFeedLikeException extends RuntimeException {
+
+    public NotMyFeedLikeException(final String message){
+        super(message);
+    }
+}
