7193544a3a55f9b6504ca46c95fb9b1906a73d1f
shinjaewon99 <tlswodnjs99@naver.com>
2023-08-08 10:02:01 +0900

    feat : 댓글 응답 DTO 클래스 추가 #83

---
+++ src/main/java/com/likelion/sns/article/comment/dto/response/CommentCommonResponse.java
@@ -0,0 +1,10 @@
+package com.likelion.sns.article.comment.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CommentCommonResponse {
+    private String message;
+}
