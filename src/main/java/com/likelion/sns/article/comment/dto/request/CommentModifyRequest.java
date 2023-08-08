1e4f575419a5178363c150f3c4989524338e2c5e
shinjaewon99 <tlswodnjs99@naver.com>
2023-08-08 10:01:45 +0900

    feat : 댓글 작성, 수정 요청 DTO 클래스 추가 #82

---
+++ src/main/java/com/likelion/sns/article/comment/dto/request/CommentModifyRequest.java
@@ -0,0 +1,10 @@
+package com.likelion.sns.article.comment.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CommentModifyRequest {
+    private String content;
+}
