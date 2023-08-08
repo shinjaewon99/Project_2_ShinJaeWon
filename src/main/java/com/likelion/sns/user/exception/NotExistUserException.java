f40555aea39a8978cbe0cf8e66e44a48f769d57c
shinjaewon99 <tlswodnjs99@naver.com>
2023-08-08 09:46:02 +0900

    remove : 비슷한 성격의 예외 클래스 삭제 #70

--- src/main/java/com/likelion/sns/user/exception/NotExistUserException.java
+++
@@ -1,8 +0,0 @@
-package com.likelion.sns.user.exception;
-
-public class NotExistUserException extends RuntimeException {
-
-    public NotExistUserException(String message) {
-        super(message);
-    }
-}
